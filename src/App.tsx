ab43eaee802686c5f6456d7e0547481f7be20122
MatthieuTirnan <matthieu.tirnan@gmail.com>
2023-05-02 15:03:56 +0200

    feat():add save img from canvas

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 import { Canvas } from "./component/canvas/canvas";
 import frameApiService from "./utils/service/frameApiService";
-
 frameApiService.addStyles(['http://localhost:4300/styles.css']);
 
 function App() {
+
   return (
     <>
       <Canvas />
--- src/component/canvas/canvas.tsx
+++ src/component/canvas/canvas.tsx
@@ -1,8 +1,6 @@
 import { fabric } from "fabric";
 import "./canvas.scss";
 import { useEffect, useState } from "react";
-import { FileUploader } from "react-drag-drop-files";
-import fileTypes from "../../utils/filetype/filetype";
 import frameApiService from "../../utils/service/frameApiService";
 import {
   AddRequestMessage,
@@ -16,10 +14,11 @@ import {
 } from "@epic-dice-studio/dice-stories-plug-ins-api";
 
 export const Canvas = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState(false);
+  const [test, setTest] = useState({})
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
   const [canvas2, setCanvas2] = useState<fabric.Canvas | null>(null);
-  const [imageExists, setImageExists] = useState(false);
+
 
   fabric.Object.prototype.cornerStyle = "circle";
   fabric.Object.prototype.cornerColor = "black";
@@ -38,7 +37,7 @@ export const Canvas = () => {
       });
 
       const canvas2 = new fabric.Canvas(canvasElement2 as any, {
-        selection: true,
+        selection: false,
         backgroundColor: "transparent",
         width: 400,
         height: 400,
@@ -47,13 +46,27 @@ export const Canvas = () => {
       setCanvas(canvas);
       canvas.on("after:render", () => {
         canvas2.clear();
-        canvas2.add(...canvas.getObjects().map((o) => o));
+        canvas2.add(...canvas.getObjects().map((o) => {
+          const toto = fabric.util.object.clone(o)
+          toto.set({
+            selectable: false,
+            evented: false,
+            lockMovementX: true,
+            lockMovementY: true,
+            lockRotation: true,
+            hasControls: false,
+            hasBorders: false
+          });
+          return toto;
+        }));
+
+
         canvas2.renderAll();
         setCanvas2(canvas2);
       });
-      frameApiService.messageReceived$.subscribe( (e) => {
+      frameApiService.messageReceived$.subscribe((e) => {
         onMessageReceived(e, canvas)
-      } );
+      });
     }
   }, []);
 
@@ -63,28 +76,27 @@ export const Canvas = () => {
         const context = message.data as PlugInContext
         const medium = context.data as { url: string }
 
-        if(medium.url){
+        if (medium.url) {
           fabric.Image.fromURL(medium.url, (oImg: fabric.Image) => {
             oImg.scaleToWidth(300);
             if (canvas) {
               canvas.add(oImg);
               oImg.center();
-              setImageExists(true);
             }
-          });
+          }, { crossOrigin: 'anonymous' });
         }
         break;
       case FrameMessageType.CLOSE:
         const url = message?.data?.url;
+        setFile(true)
         if (url) {
           fabric.Image.fromURL(url, (oImg: fabric.Image) => {
             oImg.scaleToWidth(300);
             if (canvas) {
               canvas.add(oImg);
               oImg.center();
-              setImageExists(true);
             }
-          });
+          }, { crossOrigin: 'anonymous' });
         }
         break;
 
@@ -113,32 +125,6 @@ export const Canvas = () => {
     }
   }
 
-  useEffect(() => {
-    if (file) {
-      if (imageExists) {
-        alert("Une image est déjà présente sur le canvas.");
-        return;
-      }
-    }
-  }, [file]);
-
-  // function handleChange(file: any): void {
-  //   const fileReader = new FileReader();
-  //   fileReader.onload = () => {
-  //     if (fileReader.result && canvas) {
-  //       fabric.Image.fromURL(
-  //         fileReader.result.toString(),
-  //         (oImg: fabric.Image) => {
-  //           oImg.scaleToWidth(300);
-  //           canvas.add(oImg);
-  //           oImg.center();
-  //           setImageExists(true);
-  //         }
-  //       );
-  //     }
-  //   };
-  //   fileReader.readAsDataURL(file);
-  // }
   // //partie Bordure
   // function handleChangeBorder(file: any): void {
   //   const borderReader = new FileReader();
@@ -173,7 +159,8 @@ export const Canvas = () => {
   // }
   //clip circle
   function AddCircleClip() {
-    const clipPath = new fabric.Circle({ radius: 98, opacity: 0.01 });
+    deleteClip()
+    const clipPath = new fabric.Circle({ radius: 100, opacity: 0.01 });
     if (canvas2) {
       clipPath.center();
       clipPath.lockMovementX = true;
@@ -191,22 +178,52 @@ export const Canvas = () => {
         mtr: false,
       });
       if (canvas2.width && canvas2.height) {
-        clipPath.left = canvas2.width / 2 - 98;
-        clipPath.top = canvas2.height / 2 - 98;
+        clipPath.left = canvas2.width / 2 - 100;
+        clipPath.top = canvas2.height / 2 - 100;
       }
       canvas2.clipPath = clipPath;
     }
   }
+  //add rec clipping
+  function AddRecClip() {
+    deleteClip()
+    const clipPath = new fabric.Rect({ width: 200, height: 200, opacity: 0.01 })
+    if (canvas2) {
+      clipPath.lockMovementX = true;
+      clipPath.lockMovementY = true;
+      canvas2.add(clipPath);
+      clipPath.setControlsVisibility({
+        bl: false,
+        br: false,
+        tl: false,
+        tr: false,
+        mb: false,
+        ml: false,
+        mr: false,
+        mt: false,
+        mtr: false,
+      });
+      clipPath.center()
+      canvas2.clipPath = clipPath;
+    }
+  }
+
+
   function cleanArea() {
     if (canvas) {
       canvas.clear();
-      setImageExists(false);
-      setFile(null);
+      setFile(false);
     }
   }
   function deleteClip() {
-    if (canvas) {
-      console.log("test");
+    if (canvas2 && canvas2.clipPath) {
+      canvas2.clipPath = undefined;
+      canvas2.getObjects().forEach((obj) => {
+        if (obj.clipPath || (obj instanceof fabric.Rect) || (obj instanceof fabric.Circle)) {
+          canvas2.remove(obj);
+        }
+      });
+      canvas2.renderAll();
     }
   }
 
@@ -219,46 +236,71 @@ export const Canvas = () => {
       },
     });
   }
-  function handleSave() {
-    frameApiService.sendMessage({
-      type: FrameMessageType.REQUEST,
-      data: { entityType: EntityType.MEDIA, type: RequestMessageType.ADD, data: { file: {/* "myfile" */ } } } as AddRequestMessage<{
-        file: File;
-      }>,
-    });
 
+  function handleSave() {
+    console.log(1);
+    if (canvas2 && canvas2.clipPath) {
+      const base64Url = canvas2.toDataURL({
+        format: 'png',
+        left: 100,
+        top: 100,
+        width: 200,
+        height: 200
+      })
+      console.log(2);
+      fetch(base64Url)
+        .then(res => res.blob())
+        .then(blob => {
+          const date = new Date()
+          const finalResult = new File([blob], `file ${date}`, { type: "image/png" })
+          console.log(3);
+
+          frameApiService.sendMessage({
+            type: FrameMessageType.REQUEST,
+            data: { entityType: EntityType.MEDIA, type: RequestMessageType.ADD, data: { file: finalResult } } as AddRequestMessage<{
+              file: File;
+            }>,
+          });
+          console.log(4);
+        })
+    } else {
+      alert("addClipPath for save file")
+    }
   }
   return (
     <>
-    <div className="navbar flex flex-wrap gap-3 justify-content-between align-content-center mb-3 p-2 ">
-      <div>
-        <button className="p-button p-button-text p-button-plain" onClick={handleAddFile}>
-          <i className="pi pi-plus pr-2"></i>Add file from mediathèque
-        </button>
-        <button className="p-button p-button-text p-button-plain" onClick={AddCircleClip}>
-          <i className="pi pi-plus pr-2"></i>Add circle clipping
-        </button>
-        <button className="p-button p-button-text p-button-plain" onClick={cleanArea}>
-          <i className="pi pi-eraser pr-2"></i>Clean area
-        </button>
-        <button className="p-button p-button-text p-button-plain" onClick={deleteClip}>
-          <i className="pi pi-trash pr-2"></i>Remove clip
-        </button>
+      <div className="navbar flex flex-wrap gap-3 justify-content-between align-content-center mb-3 p-2 ">
+        <div>
+          <button className="p-button p-button-text p-button-plain" onClick={handleAddFile}>
+            <i className="pi pi-plus pr-2"></i>Add file from mediathèque
+          </button>
+          <button className="p-button p-button-text p-button-plain" onClick={AddCircleClip}>
+            <i className="pi pi-plus pr-2"></i>Add circle clipping
+          </button>
+          <button className="p-button p-button-text p-button-plain" onClick={AddRecClip}>
+            <i className="pi pi-plus pr-2"></i>Add Rec clipping
+          </button>
+          <button className="p-button p-button-text p-button-plain" onClick={cleanArea}>
+            <i className="pi pi-eraser pr-2"></i>Clean area
+          </button>
+          <button className="p-button p-button-text p-button-plain" onClick={deleteClip}>
+            <i className="pi pi-trash pr-2"></i>Remove clip
+          </button>
+        </div>
+        <div>
+          <button className="p-button" onClick={handleSave}>Save</button>
+        </div>
       </div>
-      <div>
-        <button className="p-button" onClick={handleSave}>Save</button>
+      <div className={"canvas-container flex gap-3 justify-content-center mt-6"} id="canvasContainer ">
+        <div className="text-center editor">
+          <h3>Editor</h3>
+          <canvas id="canvas" />
+        </div>
+        <div className="text-center preview">
+          <h3>Preview</h3>
+          <canvas id="canvas2" />
+        </div>
       </div>
-    </div>
-    <div className={"canvas-container flex gap-3 justify-content-center mt-6"} id="canvasContainer ">
-      <div className="text-center editor">
-        <h3>Editor</h3>
-        <canvas id="canvas" />
-      </div>
-      <div className="text-center preview">
-        <h3>Preview</h3>
-        <canvas id="canvas2" />
-      </div>
-    </div>
     </>
   );
 };
